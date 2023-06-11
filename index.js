3406fc927f316e4605b423bc1a083e0efa55a2a1
areesha-usman <125805251+areesha-usman@users.noreply.github.com>
2023-06-11 17:19:34 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import { sum, sub, mul, div } from "./operation.js";
+import figlet from "figlet";
+console.log(figlet.textSync("Calculator"));
+let input = await inquirer.prompt([
+    {
+        name: "num1",
+        type: "number",
+        message: "Enter the first number: "
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "Enter the second number: "
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Select the operator you want to perform",
+        choices: ["Add", "Subtract", "Multiply", "Divide"]
+    }
+]);
+let result;
+switch (input.operator) {
+    case "Add":
+        result = sum(input.num1, input.num2);
+        console.log("the answer for addition is:" + result);
+        break;
+    case 'Subtract':
+        result = sub(input.num1, input.num2);
+        console.log("the answer for subtraction is:" + result);
+        break;
+    case 'Multiply':
+        result = mul(input.num1, input.num2);
+        console.log("the answer for multiplication is:" + result);
+        break;
+    case 'Divide':
+        result = div(input.num1, input.num2);
+        console.log("the answer of division is:" + result);
+        break;
+}
---
+++ operation.js
@@ -0,0 +1,5 @@
+const sum = (num1, num2) => { return num1 + num2; };
+const sub = (num1, num2) => { return num1 - num2; };
+const mul = (num1, num2) => { return num1 * num2; };
+const div = (num1, num2) => { return num1 / num2; };
+export { sum, sub, mul, div };
